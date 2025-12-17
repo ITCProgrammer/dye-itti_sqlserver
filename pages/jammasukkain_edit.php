312a19f2ecdbb9c21469bc9ac2212c60413e4474
tobias.mikha <tobias.sulistiyo@indotaichen.com>
2025-12-17 11:18:53 +0700

    Modal dan kondisi di halaman monitoring tempelan

--- pages/jammasukkain_edit.php
+++ pages/jammasukkain_edit.php
@@ -1,14 +1,15 @@
 <?php
 ini_set("error_reporting", 1);
 session_start();
-include("../koneksi.php");
+include ("../koneksi.php");
+include "../helpers.php";
 $modal_id = $_GET['id'];
 
-$tbl_montemp        = mysqli_query($con, "SELECT * FROM `tbl_montemp` WHERE id='$modal_id' ");
-$row_tblmontemp     = mysqli_fetch_assoc($tbl_montemp);
+$tbl_montemp        = sqlsrv_query($con, "SELECT * FROM db_dying.tbl_montemp WHERE id='$modal_id' ");
+$row_tblmontemp     = sqlsrv_fetch_array($tbl_montemp, SQLSRV_FETCH_ASSOC);
 
-$sqlCek = mysqli_query($con, "SELECT * FROM tbl_schedule WHERE id = '$row_tblmontemp[id_schedule]' ORDER BY id DESC LIMIT 1");
-$rcek = mysqli_fetch_array($sqlCek);
+$sqlCek = sqlsrv_query($con, "SELECT TOP 1 * FROM db_dying.tbl_schedule WHERE id = '$row_tblmontemp[id_schedule]' ORDER BY id DESC");
+$rcek = sqlsrv_fetch_array($sqlCek);
 
 function cekDesimal($angka){
     $bulat = round($angka);
@@ -22,8 +23,8 @@ function cekDesimal($angka){
     return $waktu;
 }
 
-$modal = mysqli_query($con, "SELECT * FROM `tbl_montemp` WHERE id='$modal_id' ");
-while ($r = mysqli_fetch_array($modal)) {
+$modal = sqlsrv_query($con, "SELECT * FROM db_dying.tbl_montemp WHERE id='$modal_id' ");
+while ($r = sqlsrv_fetch_array($modal)) {
 ?>
     <div class="modal-dialog">
         <div class="modal-content">
@@ -40,18 +41,12 @@ while ($r = mysqli_fetch_array($modal)) {
                         <div class="col-sm-4">
                             <div class="input-group date">
                                 <div class="input-group-addon"> <i class="fa fa-calendar"></i> </div>
-                                <input name="jammasukkain" value="<?= substr($r['jammasukkain'], 0, 10); ?>" type="text" class="form-control col-sm-2" required>
+                                <input name="jammasukkain" value="<?= formatDateTime($r['jammasukkain'], 'Y-m-d'); ?>" type="text" class="form-control col-sm-2" required>
                             </div>
                         </div>
                         <div class="col-sm-3">
                             <div class="input-group">
-                                <input name="tglmasukkain" type="text" class="form-control col-sm-2" id="tglmasukkain" required placeholder="00:00" pattern="[0-9]{2}:[0-9]{2}$" title=" e.g 14:25" onkeyup="
-                                                                                                                                var time = this.value;
-                                                                                                                                if (time.match(/^\d{2}$/) !== null) {
-                                                                                                                                    this.value = time + ':';
-                                                                                                                                } else if (time.match(/^\d{2}\:\d{2}$/) !== null) {
-                                                                                                                                    this.value = time + '';
-                                                                                                                                }" value="<?= substr($r['jammasukkain'], 11, 5); ?>" size="5" maxlength="5">
+                                <input name="tglmasukkain" type="text" class="form-control col-sm-2" id="tglmasukkain" required placeholder="00:00"maxlength="5"oninput="formatTime(this)"onblur="validateTime(this)"value="<?= formatDateTime($r['jammasukkain'], 'H:i'); ?>">
                                 <div class="input-group-addon">
                                 <i class="fa fa-clock-o"></i>
                                 </div>
@@ -68,3 +63,50 @@ while ($r = mysqli_fetch_array($modal)) {
         </div>
     </div>
 <?php } ?>
+
+<script>
+// Auto-format waktu (HH:MM)
+function formatTime(input) {
+    input.value = input.value.replace(/[^0-9:]/g, '');
+    let val = input.value;
+    if (val.length === 2 && !val.includes(":")) {
+        input.value = val + ":";
+    }
+
+    if (val.length > 5) {
+        input.value = val.substring(0, 5);
+    }
+}
+
+// Validasi ketika input kehilangan fokus
+function validateTime(input) {
+
+    const time = input.value;
+    if (time === "") return;
+    const regex = /^([0-9]{2}):([0-9]{2})$/;
+
+    if (!regex.test(time)) {
+        return showError(input, "Format waktu tidak valid!\nGunakan HH:MM (contoh 14:25)");
+    }
+    const [hh, mm] = time.split(":").map(Number);
+
+    if (hh < 0 || hh > 23) {
+        return showError(input, "Jam harus antara 00 sampai 23!");
+    }
+    if (mm < 0 || mm > 59) {
+        return showError(input, "Menit harus antara 00 sampai 59!");
+    }
+}
+
+// Function tampil swal error
+function showError(input, message) {
+    swal({
+        title: "Input Waktu Salah",
+        text: message,
+        icon: "error",
+    }).then(() => {
+        input.value = "";
+        input.focus();
+    });
+}
+</script>
