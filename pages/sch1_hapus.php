b9bdd8258ee4a3ad1e17c52cf14392098b842b5b
Asep Rifki <rifkipauji993@gmail.com>
2026-02-14 09:43:43 +0700

    migration: mysql to sql server

--- pages/sch1_hapus.php
+++ pages/sch1_hapus.php
@@ -2,11 +2,20 @@
 ini_set("error_reporting", 1);
 session_start();
 include("../koneksi.php");
-    $modal_id=$_GET['id'];
-    $modal1=mysqli_query($con,"UPDATE tbl_schedule SET `status`='selesai' WHERE id='$modal_id' ");
-	$modal1=mysqli_query($con,"UPDATE tbl_montemp SET `status`='selesai' WHERE id_schedule='$modal_id'");
-    if ($modal1) {
-        echo "<script>window.location='?p=Schedule-Cek';</script>";
-    } else {
-        echo "<script>alert('Gagal Hapus');window.location='?p=Schedule-Cek';</script>";
-    }
+
+$sql = sqlsrv_query($con, "
+    SELECT a.id
+    FROM db_dying.tbl_schedule a
+    INNER JOIN db_dying.tbl_montemp b ON a.id=b.id_schedule
+    WHERE ((NOT a.[status]=b.[status])
+        OR (mc_from='' AND no_urut='' AND no_mesin='' AND (a.[status]='sedang jalan' OR a.[status]='antri mesin')))
+");
+
+while($rowd = sqlsrv_fetch_array($sql, SQLSRV_FETCH_ASSOC)){
+    $modal_id = $rowd['id'];
+    $modal1 = sqlsrv_query($con, "UPDATE db_dying.tbl_schedule SET [status]='selesai' WHERE id='$modal_id'");
+    $modal1 = sqlsrv_query($con, "UPDATE db_dying.tbl_montemp SET [status]='selesai' WHERE id_schedule='$modal_id'");
+}
+
+echo "<script>window.location='?p=Schedule-Cek';</script>";
+?>
