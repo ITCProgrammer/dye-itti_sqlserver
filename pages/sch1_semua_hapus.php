23b7aec3883dac0092d69d51432490dbd89d9ba3
Nktpamungkas <nktpamungkas28@gmail.com>
2026-02-28 08:37:03 +0700

    fix: (sch1 semua hapus) Hilangkan sintaks MySQL/backtick dan ganti style SQL Server

--- pages/sch1_semua_hapus.php
+++ pages/sch1_semua_hapus.php
@@ -2,12 +2,49 @@
 ini_set("error_reporting", 1);
 session_start();
 include("../koneksi.php");
-	$sql= sqlsrv_query($con,"SELECT a.id FROM  db_dying.tbl_schedule a
-INNER JOIN  db_dying.tbl_montemp b ON a.id=b.id_schedule 
-WHERE ((not a.`status`=b.`status`) or (mc_from='' and no_urut='' and no_mesin='' and (a.`status`='sedang jalan' or a.`status`='antri mesin')))");	
-  while($rowd=sqlsrv_fetch_array($sql)){
-    $modal_id=$rowd['id'];
-    $modal1= sqlsrv_query($con,"UPDATE  db_dying.tbl_schedule SET `status`='selesai' WHERE id='$modal_id' ");
-	$modal1= sqlsrv_query($con,"UPDATE  db_dying.tbl_montemp SET `status`='selesai' WHERE id_schedule='$modal_id'");    
-  }
+
+$sql = sqlsrv_query(
+    $con,
+    "SELECT a.id
+     FROM db_dying.tbl_schedule a
+     INNER JOIN db_dying.tbl_montemp b ON a.id = b.id_schedule
+     WHERE (
+        a.[status] <> b.[status]
+        OR (
+            ISNULL(a.mc_from, '') = ''
+            AND ISNULL(CAST(a.no_urut AS VARCHAR(50)), '') = ''
+            AND ISNULL(CAST(a.no_mesin AS VARCHAR(50)), '') = ''
+            AND a.[status] IN ('sedang jalan', 'antri mesin')
+        )
+     )"
+);
+
+if ($sql !== false) {
+    while ($rowd = sqlsrv_fetch_array($sql, SQLSRV_FETCH_ASSOC)) {
+        $modal_id = isset($rowd['id']) ? (int)$rowd['id'] : 0;
+        if ($modal_id <= 0) {
+            continue;
+        }
+
+        $modal1 = sqlsrv_query(
+            $con,
+            "UPDATE db_dying.tbl_schedule SET [status] = 'selesai' WHERE id = ?",
+            array($modal_id)
+        );
+        if ($modal1 !== false) {
+            @sqlsrv_free_stmt($modal1);
+        }
+
+        $modal2 = sqlsrv_query(
+            $con,
+            "UPDATE db_dying.tbl_montemp SET [status] = 'selesai' WHERE id_schedule = ?",
+            array($modal_id)
+        );
+        if ($modal2 !== false) {
+            @sqlsrv_free_stmt($modal2);
+        }
+    }
+    @sqlsrv_free_stmt($sql);
+}
+
 echo "<script>window.location='?p=Schedule-Cek';</script>";
